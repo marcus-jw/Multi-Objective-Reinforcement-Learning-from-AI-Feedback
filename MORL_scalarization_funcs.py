d44597c8d31e27756a990de9136a92645d2fd33d
Marcus Williams <marcusjw@zoho.com>
2023-10-25 22:54:33 +0200

    added ability to normalize reward functions before scalarization

--- MORL_scalarization_funcs.py
+++ MORL_scalarization_funcs.py
@@ -1,14 +1,76 @@
 import torch
-# This class contains different Multi-Objective RL scalarization functions which will be tested
+# This file contains different Multi-Objective RL scalarization functions which will be tested
+
+scaling_dict = {
+        "conciseness":    1,
+        "ethical":        1,
+        "factual":        1,
+        "honesty":        1,
+        "legal":          1,
+        "racism":         1,
+        "relevance":      1,
+        "sexism":         1,
+        "sycophancy":     1,
+        "toxicity":       1,
+        "truthful":       1,
+        "usefulness":     1,
+        "violence":       1,
+        "x_risk":         1
+}
+offset_dict = {
+        "conciseness":    0,
+        "ethical":        0,
+        "factual":        0,
+        "honesty":        0,
+        "legal":          0,
+        "racism":         0,
+        "relevance":      0,
+        "sexism":         0,
+        "sycophancy":     0,
+        "toxicity":       0,
+        "truthful":       0,
+        "usefulness":     0,
+        "violence":       0,
+        "x_risk":         0
+    }
+
+def apply_scaling_and_offset(rewards):
+    """
+    Applies scaling and offset to the given rewards based on the scaling_dict and offset_dict.
+    
+    Parameters:
+        rewards (dict): Dictionary of rewards for all objectives.
+        
+    Returns:
+        dict: Dictionary of transformed rewards for all objectives.
+    """
+    transformed_rewards = {}
+    for key, value in rewards.items():
+        scaling = scaling_dict.get(key, 1)
+        offset = offset_dict.get(key, 0)
+        transformed_rewards[key] = value * scaling + offset
+    return transformed_rewards
 
 def scalarize_MORL(func,rewards):
+    """
+    Applies a scalarization method to a multi-objective reward set. 
+    Before scalarization, rewards are transformed based on pre-defined scaling and offset values.
+    
+    Parameters:
+        func (str): The scalarization function to apply. Valid values are currently "max_min", "soft_max_min", and "equal_weight".
+        rewards (dict): Dictionary of rewards for all objectives.
+    
+    Returns:
+        float: Scalarized reward value.
+    """
+    transformed_rewards = apply_scaling_and_offset(rewards)
     func_dict = {
         "max_min":max_min,
         "soft_max_min":soft_max_min,
         "equal_weight":equal_weight
     }
     
-    return func_dict[func](rewards)
+    return func_dict[func](transformed_rewards)
 
 def max_min(rewards):
     """
