35247f49ddeb8c3a1d94faba6c60e87f29853fea
Marcus Williams <marcusjw@zoho.com>
2023-11-03 21:24:53 +0100

    hotswapping fixes

--- LoRA_hotswapping_PM.py
+++ LoRA_hotswapping_PM.py
@@ -1,5 +1,7 @@
-from transformers import GPT2Tokenizer, GPT2Model
+from transformers import GPT2Tokenizer, GPT2Model,GPT2LMHeadModel 
 import os
+from peft import LoraConfig, TaskType, get_peft_model
+import torch
 
 class PreferenceModelHotswapper:
     """
@@ -19,25 +21,38 @@ class PreferenceModelHotswapper:
    """
     def __init__(self, base_model_name, adapter_folder):
         self.tokenizer = GPT2Tokenizer.from_pretrained(base_model_name)
-        self.model = GPT2Model.from_pretrained(base_model_name)
+        self.model = GPT2LMHeadModel.from_pretrained(base_model_name)
+        peft_config = LoraConfig(
+            task_type=TaskType.SEQ_CLS,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+        )
+    
+        self.model = get_peft_model(self.model, peft_config)
+
+
         self.adapter_names = []
         
         # Load all adapters from the given folder
         for adapter_name in os.listdir(adapter_folder):
+
             adapter_path = os.path.join(adapter_folder, adapter_name)
-            self.model.load_adapter(adapter_path)
+            # Check if the adapter name already exists and skip or handle appropriately
+            self.model.load_adapter(adapter_path,adapter_name)
             self.adapter_names.append(adapter_name)
             
     def compute_scores(self, inputs):
         scores = {}
-        
+        tok=self.tokenizer(inputs)
         # Compute score for each adapter
         for adapter_name in self.adapter_names:
             # Set the current adapter as active
-            self.model.set_active_adapters(adapter_name) #TODO measure performance of this and see if there is a better way of doing it
-            
+            self.model.set_adapter(adapter_name) #TODO measure performance of this and see if there is a better way of doing it
             # Get preference model's score
-            score = self.model(**inputs)
+            
+            score = self.model(input_ids=tok['input_ids'],attention_mask=tok['attention_mask'])
             scores[adapter_name] = score
             
         return scores
