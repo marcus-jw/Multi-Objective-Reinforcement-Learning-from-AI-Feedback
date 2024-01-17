c0699da6bdb1e8329b5371d42a9b445a0c69ce42
Marcus Williams <marcusjw@zoho.com>
2024-01-17 16:15:38 +0100

    made text bigger

--- plot_results.py
+++ plot_results.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import json
 import pandas as pd
 import seaborn as sns
+
 # Read the JSONL file
 results = []
 with open('accuracy_results.jsonl', 'r') as file:
@@ -12,11 +13,16 @@ df = pd.DataFrame(results)
 sns.set(style="whitegrid")
 
 plt.figure(figsize=(10, 6))
-ax = sns.lineplot(x="model", y="accuracy", hue="Objective function", data=df)
-ax.set_ylim(0.5, 1)  # Set y-axis range from 0.5 to 1
-plt.xlabel("Model")
-plt.ylabel("Accuracy")
+ax = sns.lineplot(x="model", y="accuracy", hue="Objective function", data=df, linewidth=2.5)
+ax.set_ylim(0.5, 1) 
+plt.title("Accuracy of the models when trained with different objective functions", fontsize=20)
+plt.xlabel("Model", fontsize=18)  
+plt.ylabel("Accuracy", fontsize=18)  
+plt.xticks(fontsize=15)  
+plt.yticks(fontsize=15)  
 plt.tight_layout() 
+
+ax.legend(fontsize=16)
+
 plt.savefig('results.png', dpi=300)
 plt.show()
-
